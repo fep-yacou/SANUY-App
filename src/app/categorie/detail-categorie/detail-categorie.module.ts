59072a02b4d1a639a2bc37e8494c88c12f1cc782
yacouba TRAORE <traore.yacouba2012@gmail.com>
2022-05-05 17:33:04 +0000

    commit du merge 05/05/2022 CategoriePage

--- src/app/categorie/detail-categorie/detail-categorie.module.ts
+++ src/app/categorie/detail-categorie/detail-categorie.module.ts
@@ -4,11 +4,8 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-<<<<<<< HEAD
-=======
 import { Ng2OrderModule } from 'ng2-order-pipe';
 
->>>>>>> 41cd1a971dd1be31601845d296b83e1472dc2bfa
 import { DetailCategoriePageRoutingModule } from './detail-categorie-routing.module';
 
 import { DetailCategoriePage } from './detail-categorie.page';
@@ -18,12 +15,8 @@ import { DetailCategoriePage } from './detail-categorie.page';
     CommonModule,
     FormsModule,
     IonicModule,
-<<<<<<< HEAD
-    DetailCategoriePageRoutingModule
-=======
     DetailCategoriePageRoutingModule,
     Ng2OrderModule
->>>>>>> 41cd1a971dd1be31601845d296b83e1472dc2bfa
   ],
   declarations: [DetailCategoriePage]
 })
